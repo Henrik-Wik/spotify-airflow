1252c071ff4b6186e79caae6d87052f05cdc951a
HenrikWik97 <henrik.wik@outlook.com>
2024-07-10 21:20:38 +0200

    Update with new columns and timestamp from raw data

--- dags/fetch_data.py
+++ dags/fetch_data.py
@@ -1,10 +1,9 @@
-import pandas as pd
 import spotipy
 import json
 
 from pg_connect import PgConnect
 from spotipy.oauth2 import SpotifyOAuth
-from datetime import timedelta, datetime, timezone
+from datetime import datetime, timezone
 from auth import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
 
 
@@ -15,32 +14,21 @@ class FetchSpotifyData:
         self.client_secret = CLIENT_SECRET
         self.redirect_uri = REDIRECT_URI
 
-    def get_latest_listened_timestamp(self):
-        max_played_at_utc = None
+    def get_latest_timestamp(self, column_name):
+        timestamp = None
         with PgConnect() as conn:
             if conn:
                 cursor = conn.cursor()
                 try:
                     cursor.execute(
-                        "SELECT MAX(played_at_utc) FROM public.spotify_songs_raw"
+                        f"SELECT MAX({column_name}) FROM public.spotify_songs_raw"
                     )
-                    max_played_at_utc = cursor.fetchall()[0][0]
+                    timestamp = cursor.fetchone()[0]
 
                 except Exception as e:
                     print(f"Error executing query: {e}")
 
-        if max_played_at_utc is None:
-            today = datetime.now()
-            previous_date = today - timedelta(days=90)
-            latest_timestamp = int(previous_date.timestamp()) * 1000
-
-        else:
-            max_played_at_utc = datetime.strptime(
-                max_played_at_utc, "%Y-%m-%dT%H:%M:%S.%fZ"
-            )
-            latest_timestamp = int(max_played_at_utc.timestamp()) * 1000
-
-        return latest_timestamp
+        return timestamp
 
     def get_data(self):
 
@@ -53,23 +41,30 @@ class FetchSpotifyData:
             )
         )
 
-        today = datetime.now()
-        yesterday = today - timedelta(days=2)
-        yesterday_unix_timestamp = int(yesterday.timestamp()) * 1000
+        latest_played_at_timestamp = self.get_latest_timestamp("played_at_timestamp")
+        if latest_played_at_timestamp == None:
+            latest_played_at_timestamp = self.get_latest_timestamp("fetched_timestamp")
 
-        latest_timestamp = self.get_latest_listened_timestamp()
-
-        song_data = sp.current_user_recently_played(after=latest_timestamp)
+        latest_unix_timestamp = int(latest_played_at_timestamp.timestamp() * 1000)
+        song_data = sp.current_user_recently_played(after=latest_unix_timestamp)
 
         song_data_raw = json.dumps(song_data)
+        fetched_timestamp_insert = datetime.now(timezone.utc)
+        played_at = [item["played_at"] for item in song_data["items"]]
+        if played_at:
+            latest_played_at_insert = max(played_at)
 
         with PgConnect() as conn:
             if conn:
                 cursor = conn.cursor()
                 try:
                     cursor.execute(
-                        "INSERT INTO spotify_songs_raw (song_data_raw) VALUES (%s)",
-                        (song_data_raw,),
+                        "INSERT INTO spotify_songs_raw (raw_json, fetched_timestamp, played_at_timestamp) VALUES (%s, %s, %s)",
+                        (
+                            song_data_raw,
+                            fetched_timestamp_insert,
+                            latest_played_at_insert,
+                        ),
                     )
                     conn.commit()
                 except Exception as e:
