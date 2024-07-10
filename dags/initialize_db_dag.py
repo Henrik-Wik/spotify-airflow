3c3099a07da0f3bcb64a611a8e8de6420ec6ec7b
HenrikWik97 <henrik.wik@outlook.com>
2024-07-10 21:02:47 +0200

    Update with sqlexecutequeryoperator and create transformed table as well

--- dags/initialize_db_dag.py
+++ dags/initialize_db_dag.py
@@ -1,7 +1,8 @@
 from airflow import DAG
-from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.utils.dates import days_ago
 from datetime import timedelta
+from sql_create_tables import CREATE_TRANSFORMED_TABLE, CREATE_RAW_TABLE
 
 
 default_args = {
@@ -10,7 +11,7 @@ default_args = {
     "start_date": days_ago(1),
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 1,
+    "retries": 0,
     "retry_delay": timedelta(minutes=5),
 }
 
@@ -18,31 +19,34 @@ default_args = {
 with DAG(
     dag_id="init_db_dag",
     schedule_interval=None,
-    max_active_runs=1,
+    max_active_runs=2,
     catchup=False,
     default_args=default_args,
 ) as dag:
 
-    create_raw_table = PostgresOperator(
-        task_id="create_tables",
-        postgres_conn_id="postgres_localhost",
+    create_raw_table = SQLExecuteQueryOperator(
+        task_id="create_raw_table", conn_id="postgres_localhost", sql=CREATE_RAW_TABLE
+    )
+
+    create_transformed_table = SQLExecuteQueryOperator(
+        task_id="create_transformed_table",
+        conn_id="postgres_localhost",
+        sql=CREATE_TRANSFORMED_TABLE,
+    )
+
+    insert_sample_data = SQLExecuteQueryOperator(
+        task_id="insert_sample_data",
+        conn_id="postgres_localhost",
         sql="""
-        CREATE TABLE IF NOT EXISTS spotify_songs_raw (
-            id SERIAL PRIMARY KEY,
-            raw_json JSONB,
-            processed BOOLEAN DEFAULT FALSE,
-            played_at_utc TIMESTAMPTZ
+        INSERT INTO spotify_songs_raw (raw_json, processed, fetched_timestamp, played_at_timestamp) 
+        VALUES (
+            '{"items": [{"played_at": "2023-07-08T10:00:00Z", "track": {"name": "Sample Song", "album": {"artists": [{"name": "Sample Artist"}], "images": [{"url": ""}, {"url": "http://sample_url"}], "name": "Sample Album", "id": "sample_album_id"}, "duration_ms": 180000, "external_urls": {"spotify": "http://sample_song_link"}, "artists": [{"id": "sample_artist_id"}], "id": "sample_track_id"}}]}'
+            , TRUE
+            , '2023-07-08T10:00:00Z'
+            , '2023-07-08T10:00:00Z'
         );
         """,
-    )
-
-    insert_sample_data = PostgresOperator(
-        task_id="insert_sample_data",
-        postgres_conn_id="postgres_localhost",
-        sql="""
-        INSERT INTO spotify_songs_raw (raw_json, played_at_utc) VALUES
-        ('{"items": [{"played_at": "2023-07-08T10:00:00Z", "track": {"name": "Sample Song", "album": {"artists": [{"name": "Sample Artist"}], "images": [{"url": ""}, {"url": "http://sample_url"}], "name": "Sample Album", "id": "sample_album_id"}, "duration_ms": 180000, "external_urls": {"spotify": "http://sample_song_link"}, "artists": [{"id": "sample_artist_id"}], "id": "sample_track_id"}}]}', '2023-07-08T10:00:00Z');
-        """,
-    )
+    )  # set processed to true so the sample data doesn't get included later.
 
     create_raw_table >> insert_sample_data
+    create_transformed_table
