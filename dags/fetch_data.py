aa55a2cc3fc81d79899b8a116080dec0b7ca6660
HenrikWik97 <henrik.wik@outlook.com>
2024-07-11 16:27:18 +0200

    Update with function to get access token and error message

--- dags/fetch_data.py
+++ dags/fetch_data.py
@@ -13,6 +13,18 @@ class FetchSpotifyData:
         self.client_id = CLIENT_ID
         self.client_secret = CLIENT_SECRET
         self.redirect_uri = REDIRECT_URI
+        # self.access_token = ""
+
+    def get_access_token(self):
+        auth_manager = SpotifyOAuth(
+            client_id=self.client_id,
+            client_secret=self.client_secret,
+            redirect_uri=self.redirect_uri,
+            scope="user-read-private user-library-read user-read-recently-played",
+            cache_path="/opt/airflow/config/.cache",
+        )
+
+        return auth_manager
 
     def get_latest_timestamp(self, column_name):
         timestamp = None
@@ -31,15 +43,8 @@ class FetchSpotifyData:
         return timestamp
 
     def get_data(self):
-
-        sp = spotipy.Spotify(
-            auth_manager=SpotifyOAuth(
-                client_id=self.client_id,
-                client_secret=self.client_secret,
-                redirect_uri=self.redirect_uri,
-                scope="user-read-private user-library-read user-read-recently-played",
-            )
-        )
+        auth_manager = self.get_access_token()
+        sp = spotipy.Spotify(auth_manager=auth_manager)
 
         latest_played_at_timestamp = self.get_latest_timestamp("played_at_timestamp")
         if latest_played_at_timestamp == None:
@@ -51,8 +56,12 @@ class FetchSpotifyData:
         song_data_raw = json.dumps(song_data)
         fetched_timestamp_insert = datetime.now(timezone.utc)
         played_at = [item["played_at"] for item in song_data["items"]]
+
         if played_at:
             latest_played_at_insert = max(played_at)
+        else:
+            print("Error, no new music to fetch.")
+            latest_played_at_insert = None
 
         with PgConnect() as conn:
             if conn:
